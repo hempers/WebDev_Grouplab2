9073cf91b7188f6b67c27d9f97048908ce2aea21
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2024-09-02 15:00:30 +0800

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/content', function () {
-    return view('content');
-});
+
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/home', function () {
+    return redirect('/');
+});
+
+Route::get('/contactus', function () {
+    return view('contactus');
 });
