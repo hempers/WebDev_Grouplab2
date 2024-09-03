7b480f002270fc8d14268757a1e3bfcdb43a7645
richardbilan <chardzbilan@gmail.com>
2024-09-03 19:52:38 +0800

    final update??

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,67 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <meta charset="UTF-8">
     <title>About Us</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Staatliches&display=swap'>
     <link rel="stylesheet" href="styles/styles_a.css">
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
 </head>
-
 <body>
-    <header>
-        <nav class="logo">
-            <ul>
-                <li><a href="{{ url('/home')}}">Home</a></li>
-                <li><a href="{{ url('/about')}}">About Us</a></li>
-                <li><a href="{{ url('/content')}}">Content</a></li>
-            </ul>
-        </nav>
-    </header>
+<header>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+    <div class="section-header">
+    <h2 class="header header_home">About Us</h2>
+</div>
 
-    <div id="about" class="first_page">
-        <div class="wrapper">
-            <div class="header header_home">About Us</div>
-            <div class="info-container">
-                <div class="info-wrapper">
-                    <div class="info-text">
-                        <h2>Name: Richard D. Bilan</h2>
-                        <h2>Address: P4 Brgy 1 Pob. Camalig, Albay</h2>
-                        <h2>Year & Course: 3rd Year, BS Information Technology</h2>
-                        <p>I am an IT student with a strong curiosity about the tech field. While I am still exploring my future career path, I am committed to completing my studies and am passionate about learning and discovering new aspects of technology.</p>
-                    </div>
-                    <div class="info-image">
-                        <img src="assets/img/about/richard.jpg" alt="Richard D. Bilan">
-                    </div>
-                </div>
-
-                <div class="info-wrapper">
-                    <div class="info-text">
-                        <h2>Name: Mae Anne B. Bongat</h2>
-                        <h2>Address: Zone 2 Bogñabong Tabaco City, Albay</h2>
-                        <h2>Year & Course: 3rd Year, BS Information Technology</h2>
-                        <p>A student eager to learn more about the IT industry. Although still uncertain about which path to take, I am committed to completing everything needed to graduate.</p>
-                    </div>
-                    <div class="info-image">
-                        <img src="assets/img/about/meh.jpg" alt="Mae Anne Bongat">
-                    </div>
-                </div>
-
-                <div class="info-wrapper">
-                    <div class="info-text">
-                        <h2>Name: Althea M. Casimiro</h2>
-                        <h2>Address: Purok 5 Masarawag Guinobatan, Albay</h2>
-                        <h2>Year & Course: 3rd Year, BS Information Technology</h2>
-                        <p>I'm an IT student who is passionate about finishing my studies in this course, but I also love engaging in creative activities that I find interesting and enjoyable during my free time.</p>
-                    </div>
-                    <div class="info-image">
-                        <img src="assets/img/about/thei.jpg" alt="Althea M. Casimiro">
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="container">
+    <div class="box">
+    <div class="info-image">
+        <img src="assets/img/about/richard.jpg" alt="Richard D. Bilan">
+    </div>
+    <h3> Richard D. Bilan</h3>
+    <h3> P4 Brgy 1 Pob. Camalig, Albay</h3>
+    <h3>3rd Year, BS Information Technology</h3>
+    <p>I am an IT student with a strong curiosity about the tech field. While I am still exploring my future career path, I'm focused on finishing my studies and excited to learn more about technology.</p>
+</div>
+<div class="box">
+    <div class="info-image">
+        <img src="assets/img/about/meh.jpg" alt="Mae Anne Bongat">
+    </div>
+    <h3> Mae Anne B. Bongat</h3>
+    <h3> Zone 2 Bogñabong Tabaco City, Albay</h3>
+    <h3> 3rd Year, BS Information Technology</h3>
+    <p>A student eager to learn more about the IT industry. Although still uncertain about which path to take, I am committed to completing everything needed to graduate.</p>
+</div>
+<div class="box">
+    <div class="info-image">
+        <img src="assets/img/about/thei.jpg" alt="Althea M. Casimiro">
+    </div>
+    <h3> Althea M. Casimiro</h3>
+    <h3> Purok 5 Masarawag Guinobatan, Albay</h3>
+    <h3> 3rd Year, BS Information Technology</h3>
+    <p>I'm an IT student who is passionate about finishing my studies in this course, but I also love engaging in creative activities that I find interesting and enjoyable during my free time.</p>
+</div>
     </div>
 </body>
-
 </html>
