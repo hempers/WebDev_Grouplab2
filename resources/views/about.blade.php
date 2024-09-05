b884926acc421f0c1495873f1d4eeafd3521bf37
richardbilan <157667872+richardbilan@users.noreply.github.com>
2024-09-05 23:27:37 +0800

    pinaka finall updatee!

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,17 +9,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Staatliches&display=swap'>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;family=Figtree:wght@300;600&amp;display=swap'>
     <link rel="stylesheet" href="styles/styles_a.css">
 </head>
 <body>
 <header>
 <nav>
     <ul>
-        <!-- Home link -->
         <li><a href="{{ url('/home') }}">Home</a></li>
-        <!-- About Us link -->
         <li><a href="{{ url('/about') }}">About Us</a></li>
-        <!-- Content link -->
         <li><a href="{{ url('/content') }}">Content</a></li>
     </ul>
 </nav>
@@ -34,28 +32,28 @@
     <div class="info-image">
         <img src="assets/img/about/richard.jpg" alt="Richard D. Bilan">
     </div>
-    <h3> Richard D. Bilan</h3>
+    <h3> RICHARD D. BILAN JR. </h3>
     <h3> P4 Brgy 1 Pob. Camalig, Albay</h3>
-    <h3>3rd Year, BS Information Technology</h3>
+    <h3> BS Information Technology 3rd Year</h3>
     <p>I am an IT student with a strong curiosity about the tech field. While I am still exploring my future career path, I'm focused on finishing my studies and excited to learn more about technology.</p>
 </div>
 <div class="box">
     <div class="info-image">
         <img src="assets/img/about/meh.jpg" alt="Mae Anne Bongat">
     </div>
-    <h3> Mae Anne B. Bongat</h3>
+    <h3> MAE ANNE B. BONGAT</h3>
     <h3> Zone 2 Bogñabong Tabaco City, Albay</h3>
-    <h3> 3rd Year, BS Information Technology</h3>
+    <h3>  BS Information Technology 3rd Year</h3>
     <p>A student eager to learn more about the IT industry. Although still uncertain about which path to take, I am committed to completing everything needed to graduate.</p>
 </div>
 <div class="box">
     <div class="info-image">
         <img src="assets/img/about/thei.jpg" alt="Althea M. Casimiro">
     </div>
-    <h3> Althea M. Casimiro</h3>
+    <h3> ALTHEA M. CASIMIRO</h3>
     <h3> Purok 5 Masarawag Guinobatan, Albay</h3>
-    <h3> 3rd Year, BS Information Technology</h3>
-    <p>I'm an IT student who is passionate about finishing my studies in this course, but I also love engaging in creative activities that I find interesting and enjoyable during my free time.</p>
+    <h3>  BS Information Technology 3rd Year</h3>
+    <p>I'm an IT student who is passionate about finishing my studies in this course but I also love engaging in creative activities that I find interesting and enjoyable during my free time.</p>
 </div>
     </div>
 </body>
