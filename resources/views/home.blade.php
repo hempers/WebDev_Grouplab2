76ab18c498a1c8943086914421742dc3d8764e4f
richardbilan <157667872+richardbilan@users.noreply.github.com>
2024-09-05 22:22:30 +0800

    final final finall updatee!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,16 @@
     <title>Web Development</title>
     <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;family=Figtree:wght@300;600&amp;display=swap'>
     <link rel="stylesheet" href="styles/styles_h.css">
+    <!-- Add Bootstrap CSS for modal styling -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 
     <header>
-    <div class="logo">
-        <a href="{{ url('/') }}" style="text-decoration: none;">
-            <h1 class="text">Laravel</h1>
-        </a>
+        <div class="logo">
+            <a href="{{ url('/') }}" style="text-decoration: none;">
+                <h1 class="text">Laravel</h1>
+            </a>
         </div>
         <nav>
             <ul>
@@ -29,10 +31,39 @@
         <h1>Web Development</h1>
         <h2>First Laravel Project</h2>
         <a href="{{ url('/content')}}">
-        <button>Explore More</button>
+            <button>Explore More</button>
         </a>
-        
     </div>
 
+    <!-- Modal -->
+    <div class="modal fade" id="welcomeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Welcome</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Welcome, {{ session('user_name', 'Guest') }}!</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery and Bootstrap JS for modal functionality -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    <script>
+        $(document).ready(function() {
+            $('#welcomeModal').modal('show');
+        });
+    </script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,4 +13,16 @@ Route::get('/content', function () {
 });
 Route::get('/about', function () {
     return view('about');
+});
+
+
+
+
+Route::get('/user/{name?}', function ($name = null) {
+    // Store the name in the session, default to 'Guest' if no name is provided
+    $userName = $name ?: 'Guest';
+    session()->flash('user_name', $userName);
+
+    // Redirect back to the home page
+    return redirect('/');
 });
