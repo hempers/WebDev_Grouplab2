765012d9070a763199f630dbaf72d312c26fe4fd
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2024-09-06 13:36:26 +0800

    deleted some assets

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,37 +1,61 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
+
 <head>
-  <meta charset="UTF-8">
-  <title>About Me</title>
-  <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;family=Figtree:wght@300;600&amp;display=swap'>
-  <link rel="stylesheet" href="styles/styles_a.css">
-  <meta content="width=device-width, initial-scale=1" name="viewport" />
-  
+    <meta charset="UTF-8">
+    <title>About Us</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Staatliches&display=swap'>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;family=Figtree:wght@300;600&amp;display=swap'>
+    <link rel="stylesheet" href="styles/styles_a.css">
 </head>
 <body>
-  <header>
-  <div class="logo">
-        <a href="{{ url('/') }}" style="text-decoration: none;">
-          <h1 class="text">Laravel</h1>
-        </a>
-  </div>
-    <nav class="logo">
+
+<header>
+    <nav class="list">
         <ul>
-        <li><a href="{{ url('/home')}}">Home</a></li>
+            <li><a href="{{ url('/')}}">Home</a></li>
             <li><a href="{{ url('/about')}}">About</a></li>
-            <li><a href="{{ url('/content')}}">Content</a></li>
+            <li><a href="{{ url('/contactus')}}">Contact Us</a></li>
         </ul>
     </nav>
 </header>
-<div id="home" class="first_page">
-  <div class="wrapper">
-    <div class="header header_home">Hi, I'm Mae Anne</div>
-    <div class="content content_home">Hello! I'm Mae Anne B. Bongat, a third-year BS Information Technology student eager to learn more about the IT industry. Although I'm still uncertain about which path to take, I'm committed to completing everything needed to graduate from college.</div>
-    <div class="image">
-     
+
+    <div class="section-header">
+    <h2 class="header header_home">About Us</h2>
+</div>
+
+    <div class="container">
+    <div class="box">
+    <div class="info-image">
+        <img src="assets/img/about/richard.jpg" alt="Richard D. Bilan">
     </div>
-  </div>
+    <h2> RICHARD D. BILAN JR. </h2>
+    <h3> Purok 4 Brgy 1 Pob. Camalig, Albay</h3>
+    <h3> BS Information Technology <br> 3rd Year Student</h3>
+    <p>I am an IT student with a strong curiosity about the tech field. While I am still exploring my future career path, I'm focused on finishing my studies and excited to learn more about technology.</p>
 </div>
+<div class="box">
+    <div class="info-image">
+        <img src="assets/img/about/meh.jpg" alt="Mae Anne Bongat">
+    </div>
+    <h2> MAE ANNE B. BONGAT</h2>
+    <h3> Purok 2 Bogñabong Tabaco City, Albay</h3>
+    <h3>  BS Information Technology<br> 3rd Year Student</h3>
+    <p>A student eager to learn more about the IT industry. Although still uncertain about which path to take, I am committed to completing everything needed to graduate.</p>
 </div>
+<div class="box">
+    <div class="info-image">
+        <img src="assets/img/about/thei.jpg" alt="Althea M. Casimiro">
+    </div>
+    <h2> ALTHEA M. CASIMIRO</h2>
+    <h3> Purok 5 Masarawag Guinobatan, Albay</h3>
+    <h3>  BS Information Technology<br> 3rd Year Student</h3>
+    <p>I'm an IT student who is passionate about finishing my studies in this course but I also love engaging in creative activities that I find interesting and enjoyable during my free time.</p>
+</div>
+    </div>
 </body>
 </html>
