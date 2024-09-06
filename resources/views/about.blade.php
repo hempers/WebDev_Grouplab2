6888200be789ecbd6231651674c43d041684f3e4
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2024-09-06 14:48:48 +0800

    finishing of about us page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -29,32 +29,36 @@
 </div>
 
     <div class="container">
-    <div class="box">
-    <div class="info-image">
-        <img src="assets/img/about/richard.jpg" alt="Richard D. Bilan">
-    </div>
-    <h2> RICHARD D. BILAN JR. </h2>
-    <h3> Purok 4 Brgy 1 Pob. Camalig, Albay</h3>
-    <h3> BS Information Technology <br> 3rd Year Student</h3>
-    <p>I am an IT student with a strong curiosity about the tech field. While I am still exploring my future career path, I'm focused on finishing my studies and excited to learn more about technology.</p>
-</div>
+        <div class="box">
+            <div class="info-image">
+                <img src="assets/img/about/richard.jpg" alt="Richard D. Bilan">
+            </div>
+            <h2> RICHARD D. BILAN JR. </h2>
+                <ul class="info-list">
+                    <li><strong>Education:</strong> <br>3rd Year BS Information Technology <br> College of Science <br> Bicol University</li>
+                    <li><strong>About Me:</strong> <br>I am an IT student with a strong curiosity about the tech field. While I am still exploring my future career path, I'm focused on finishing my studies and excited to learn more about technology.</li>
+                </ul>
+        </div>
+
 <div class="box">
     <div class="info-image">
         <img src="assets/img/about/meh.jpg" alt="Mae Anne Bongat">
     </div>
-    <h2> MAE ANNE B. BONGAT</h2>
-    <h3> Purok 2 Bogñabong Tabaco City, Albay</h3>
-    <h3>  BS Information Technology<br> 3rd Year Student</h3>
-    <p>A student eager to learn more about the IT industry. Although still uncertain about which path to take, I am committed to completing everything needed to graduate.</p>
-</div>
+        <h2> MAE ANNE B. BONGAT</h2>
+        <ul class="info-list">
+                    <li><strong>Education:</strong><br>3rd Year BS Information Technology <br> College of Science <br> Bicol University</li>
+                    <li><strong>About Me:</strong> <br>A student eager to learn more about the IT industry. Although still uncertain about which path to take, I am committed to completing everything needed to graduate.</li>
+                </ul>
+    </div>
 <div class="box">
     <div class="info-image">
         <img src="assets/img/about/thei.jpg" alt="Althea M. Casimiro">
     </div>
-    <h2> ALTHEA M. CASIMIRO</h2>
-    <h3> Purok 5 Masarawag Guinobatan, Albay</h3>
-    <h3>  BS Information Technology<br> 3rd Year Student</h3>
-    <p>I'm an IT student who is passionate about finishing my studies in this course but I also love engaging in creative activities that I find interesting and enjoyable during my free time.</p>
+        <h2> ALTHEA M. CASIMIRO</h2>
+                <ul class="info-list">
+                    <li><strong>Education:</strong><br>3rd Year BS Information Technology <br> College of Science <br> Bicol University</li>
+                    <li><strong>About Me:</strong> <br>I'm an IT student who is passionate about finishing my studies in this course but I also love engaging in creative activities that I find interesting and enjoyable during my free time.</li>
+                </ul>
 </div>
     </div>
 </body>
