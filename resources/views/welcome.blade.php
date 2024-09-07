0e8398a5c707451f531a0877a520aea49d14dd7f
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2024-09-07 14:54:33 +0800

    adjusted modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Development</title>
-    <link rel="stylesheet" href="styles/styles_h.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" >
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Staatliches&display=swap'>
     <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;family=Figtree:wght@300;600&amp;display=swap'>
+    <link rel="stylesheet" href="styles/styles_h.css">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 
@@ -19,7 +22,6 @@
                 <h1 class="text">Laravel</h1>
             </a>
         </div>
-        
         <nav>
             <ul>
                 <li><a href="{{ url('/')}}" style="text-decoration: none;">Home</a></li>
@@ -29,31 +31,32 @@
         </nav>
     </header>
 
-        <div class="container">
-            <img src="assets/img/home/logo.svg" alt="Laravel Logo" class="logo">
-                <h1>Web Development</h1>
-                <h2>First Laravel Project</h2>
-                    <a href="{{ url('/about')}}">
-                        <button>Explore More</button>
-                    </a>
-        </div>
+    <div class="container">
+        <img src="assets/img/home/logo.svg" alt="Laravel Logo" class="logo">
+        <h1 style="font-weight: 900;">Web Development</h1>
+        <h3 style="font-weight: 100;">Group Laravel Project</h3>
+        <a href="{{ url('/content')}}">
+            <button>Explore More</button>
+        </a>
+    </div>
 
     <section class="welcome-section">
-        <div class="modal fade welcome-modal" id="welcomeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <p>Welcome, {{ session('user_name') }}!</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Get Started</button>
-                    </div>
+    <!-- Modal -->
+    <div class="modal fade welcome-modal" id="welcomeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <p>Welcome, {{ session('user_name') }}!</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Get Started</button>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-   
+    <!-- jQuery and Bootstrap JS for modal functionality -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -71,6 +74,5 @@
             @endif
         });
     </script>
-    
 </body>
 </html>
